a4dbb8a703531b3fc4b3d6ee8ea33ac442b6e0e4
Alexus91 <mrfadlkamal@gmail.com>
2023-06-06 14:43:55 -0700

    3. When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for lett in range(97, 123):
-    if chr(lett) is not 'q' and chr(lett) is not 'e':
+    if chr(lett) != 'q' and chr(lett) != 'e':
         print("{}".format(chr(lett)), end="")
