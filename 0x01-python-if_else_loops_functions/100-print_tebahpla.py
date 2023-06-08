b11b16044a1a169a05d0979e63e4dfee38cc4120
Alexus91 <mrfadlkamal@gmail.com>
2023-06-07 18:25:15 -0700

    fixing the output

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,7 +2,7 @@
 """ prints the ASCII alphabet, in reverse order, alternating lowercase and uppercase
 (z in lowercase and Y in uppercase)"""
 ind = 0
+x = ""
 for char in range(ord('z'), ord('A') - 1, -1):
-    x = chr(char - ind)
-    print("{}".format(x), end="")
+    x += chr(char - ind)
     ind = 32 if ind == 0 else 0
