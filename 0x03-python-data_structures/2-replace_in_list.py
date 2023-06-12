8a92ac4f008ee66269db6ad2670657d2b5c1a1b8
Alexus91 <mrfadlkamal@gmail.com>
2023-06-12 16:48:26 -0700

    try to fixu out put by return to last code

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     """replaces an element of a list at a specific position"""
-    if idx < 0 or idx >= len(my_list):
-        return (my_list)
-    nw_list = my_list[:]
-    nw_list[idx] = element
-    return (nw_list)
+    if idx >= 0 and idx < len(my_list):
+        my_list[idx] = element
+    return (my_list)
