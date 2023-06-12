95e1cdc1082685c2ad33346129743de86a014d44
Alexus91 <mrfadlkamal@gmail.com>
2023-06-12 15:57:17 -0700

    fix output  0. Print a list of integers

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,5 +2,5 @@
 def print_list_integer(my_list=[]):
     """ prints all integers of a list """
     for idx in range(len(my_list)):
-        print("{0:d}".format(my_list[idx]))
+        print("{:d}".format(my_list[idx]))
 
