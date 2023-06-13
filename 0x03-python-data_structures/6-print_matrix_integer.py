3ef36adbc769202b36926d16d7eb91b1a5bf7d18
Alexus91 <mrfadlkamal@gmail.com>
2023-06-12 17:59:06 -0700

    fixing some issue in my syntax task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,5 @@ def print_matrix_integer(matrix=[[]]):
     """prints a matrix of integers."""
     for w in matrix:
         for c in w:
-            g = (if c != w[-1] else "")
-            print("{:d}".format(c), end=" ", g)
+            print("{:d}".format(c), end=" " if c != w[-1] else "")
     print()
