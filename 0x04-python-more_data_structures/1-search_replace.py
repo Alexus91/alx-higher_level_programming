141e342d9d53006b576a28859fa151a53ad61820
Alexus91 <mrfadlkamal@gmail.com>
2023-06-14 17:29:24 -0700

    fixing the requ

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-
-    nwlist= list(map(lambda x: replace if x == search else x, my_list))
-    return (nwlist)
+    new = list(map(lambda x: replace if x == search else x, my_list))
+    return (new)
