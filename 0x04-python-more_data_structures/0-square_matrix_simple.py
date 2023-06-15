f5555330ffc7ccff67a75e8172afbbf8a81e194a
Alexus91 <mrfadlkamal@gmail.com>
2023-06-14 17:17:50 -0700

    fixing output

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    nw_mtx = matrix.copy()
-    for indx in range(len(matrix)):
-        nw_mtx[indx] = list(map(lambda x: x**2, matrix[indx]))
-        return (nw_mtx)
+    """computes the square value of all integers of a matrix."""
+    new_matrix = matrix.copy()
+
+    for idx in range(len(matrix)):
+        new_matrix[idx] = list(map(lambda x: x**2, matrix[idx]))
+
+    return (new_matrix)
