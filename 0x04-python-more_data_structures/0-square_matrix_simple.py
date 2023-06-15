716bda7f4efc76fc7191f71c915cc325fa85eb36
Alexus91 <mrfadlkamal@gmail.com>
2023-06-14 17:11:38 -0700

    fix the output by deleting a 2 was messlockated

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,7 +2,6 @@
 def square_matrix_simple(matrix=[]):
     """ computes the square value of all integers of a matrix."""
     nw_mtx = matrix.copy()
-
     for indx in range(len(matrix)):
-        nw_mtx[indx] = list(map(lambda x: x**2, matrix[indx]))2
+        nw_mtx[indx] = list(map(lambda x: x**2, matrix[indx]))
         return (nw_mtx)
