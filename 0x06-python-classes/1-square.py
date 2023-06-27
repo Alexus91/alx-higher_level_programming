4e5e130d62efe36049a854edb13da4449b76da3c
Alexus91 <mrfadlkamal@gmail.com>
2023-06-27 11:07:42 -0700

    1. Square with size  fixing requ

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-"""Define a class Square"""
+"""Define a class Square."""
+
 
 class Square:
-    """a square"""
-    
-    def __init__(self, size):
-        """initialize a new square"""
+    """a square."""
 
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): size of the new square.
+        """
         self.__size = size
