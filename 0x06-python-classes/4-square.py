47638b387b8d084655fecfaa09f43ae35d425c19
Alexus91 <mrfadlkamal@gmail.com>
2023-06-27 13:08:29 -0700

    fixing reque

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Define a class Square."""
 
+
 class Square:
     """Represent a square."""
 
@@ -28,4 +29,3 @@ class Square:
     def area(self):
         """Return the current area of the square."""
         return (self.__size * self.__size)
-
