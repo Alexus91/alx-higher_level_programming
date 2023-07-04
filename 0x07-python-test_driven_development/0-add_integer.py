6efc38f861074ce7c860b063a43553fa4580811c
Alexus91 <mrfadlkamal@gmail.com>
2023-07-03 22:32:03 -0700

    test passed everything cool for the 2 first tasks :0. Integers addition and  1. Divide a matrix

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""integer addition function."""
+
+
+def add_integer(a, b=98):
+    """Return the integer addition of a and b.
+
+    Float arguments are typecasted to ints before addition is performed.
+
+    Raises:
+        TypeError: If either of a or b are  non-integer
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    
+    return int(a) + int(b)
