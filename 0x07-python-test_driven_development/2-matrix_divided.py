2e41f2ad53c14db299d293e856a37a6cb68ed3cf
Alexus91 <mrfadlkamal@gmail.com>
2023-07-03 23:27:14 -0700

    doing task (2. Say my name) and also change the code of ( 1. Divide a matrix ) to be fit with pycodestyle lines tall requirements

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -4,10 +4,12 @@
 
 def matrix_divided(matrix, div):
     """Divide all elements of a matrix."""
+    if (not isinstance(matrix, list) or matrix == [] or
+            not all(isinstance(row, list) for row in matrix) or
+            not all((isinstance(el, int) or isinstance(el, float))
+                    for el in [num for row in matrix for num in row])):
 
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
-
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix of integers/floats")
 
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
