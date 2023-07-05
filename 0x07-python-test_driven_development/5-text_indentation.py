e9a89348a5828b53ca14a494fa689e98b0578928
Alexus91 <mrfadlkamal@gmail.com>
2023-07-04 19:03:28 -0700

    doing task ( 3. Print square  4. Text indentation )

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""text-indentation function."""
+def text_indentation(text):
+    """""Print text with two new lines after each '.', '?', and ':'."""
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    b = 0
+    while b < len(text) and text[b] == ' ':
+        b += 1
+
+    while b < len(text):
+        print(text[b], end="")
+        if text[b] == "\n" or text[b] in ".?:":
+            if text[b] in ".?:":
+                print("\n")
+            b += 1
+            while b < len(text) and text[b] == ' ':
+                b += 1
+            continue
+        b += 1
