b7e2f2a8bbdab907cba265359eae72f04eefdd8d
Alexus91 <mrfadlkamal@gmail.com>
2023-07-11 19:23:26 -0700

    9. Student to JSON

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,13 @@
+class Student:
+    """Defines a class Student."""
+
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Get a dictionary representation of the Student."""
+        return self.__dict__
