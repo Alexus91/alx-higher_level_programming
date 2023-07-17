17a2a99dcd4f12454f93aafe5148e08ec71bcf01
Alexus91 <mrfadlkamal@gmail.com>
2023-07-17 15:56:30 -0700

    16. JSON string to file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -21,3 +21,14 @@ class Base:
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return ("[]")
         return (json.dumps(list_dictionaries))
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as file:
+            if list_objs is None:
+                file.write("[]")
+            else:
+                dictionaries = [obj.to_dictionary() for obj in list_objs]
+                json_string = cls.to_json_string(dictionaries)
+                file.write(json_string)
