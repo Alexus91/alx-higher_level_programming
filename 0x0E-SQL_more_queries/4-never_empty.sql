d5212965e6d609f7cb7dc212aecd9a96f382f59f
Alexus91 <mrfadlkamal@gmail.com>
2023-08-16 17:44:12 -0700

    6. States table

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,8 +1,5 @@
-USE `hbtn_0d_2`;
-
--- Create table if not exists
-CREATE TABLE IF NOT EXISTS `id_not_null` (
-    `id` INT NOT NULL DEFAULT 1,
-    `name` VARCHAR(256),
-    PRIMARY KEY (`id`)
-);
+-- create table
+CREATE TABLE IF NOT EXISTS id_not_null (
+    id INT DEFAULT 1,
+    name VARCHAR(256)
+) ENGINE=INNODB;
