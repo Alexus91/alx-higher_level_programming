96ec1ecc5ff8ae0ef82f022d367387e9d290127e
Alexus91 <mrfadlkamal@gmail.com>
2023-08-16 17:40:53 -0700

    fixing 3-force_name.sql

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,8 +1,5 @@
-USE `hbtn_0d_2`;
-
--- Create table if not exists
-CREATE TABLE IF NOT EXISTS `force_name` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(256) NOT NULL,
-    PRIMARY KEY (`id`)
-);
+-- create table
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL
+) ENGINE=INNODB;
