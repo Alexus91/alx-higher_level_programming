69eeafe6577e21a00a28cf226b527a44ce61811b
Alexus91 <mrfadlkamal@gmail.com>
2023-09-11 18:18:26 -0700

    fixing semistandard

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 const lines = [
-  "C is fun",
-  "Python is cool",
-  "JavaScript is amazing"
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
 ];
-
 for (let inx = 0; inx < lines.length; inx++) {
   console.log(lines[inx]);
 }
