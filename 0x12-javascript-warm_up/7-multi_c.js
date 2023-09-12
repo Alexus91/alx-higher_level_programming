c42e8dfdf4543175156973708fb72b7fb25360c8
Alexus91 <mrfadlkamal@gmail.com>
2023-09-11 18:49:52 -0700

    7. I love C

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const numOccurrences = Number(process.argv[2]);
+const numOccurrences = parseInt(process.argv[2]);
 
 if (!isNaN(numOccurrences) && numOccurrences > 0) {
   for (let i = 0; i < numOccurrences; i++) {
-    console.log('C is fun';
+    console.log('C is fun');
   }
 } else {
   console.log('Missing number of occurrences');
