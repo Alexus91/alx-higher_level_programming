53ad8a0d5b726df3ff19fa702b89011428225234
Alexus91 <mrfadlkamal@gmail.com>
2023-09-17 11:52:21 -0700

    fixing pycodesytle

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,8 +7,9 @@ from sys import argv
 from model_state import Base, State
 
 if __name__ == "__main__":
-    engi = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(argv[1], argv[2], argv[3]))
+    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                                                                    argv[2],
+                                                                    argv[3]))
     Base.metadata.create_all(engi)
     sess = sessionmaker(bind=engi)()
     for state in sess.query(State).order_by(State.id):
