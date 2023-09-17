b6c09c774f3287a7dde907a1783a3d21988a9fe0
Alexus91 <mrfadlkamal@gmail.com>
2023-09-17 11:31:49 -0700

    5. All cities by state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -6,13 +6,15 @@ with specified state name
 
 import MySQLdb
 from sys import argv
+
+
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=argv[1], passwd=argv[2], db=argv[3])
     curs = db.curs()
     curs.execute("""SELECT cities.name
                    FROM cities JOIN states ON cities.state_id = states.id
-                   WHERE states.name = %(state)s""", {'state': argv[4]})
+                   WHERE states.name = %(state)s""", (argv[4],))
     data = curs.fetchall()
     print(", ".join(city[0] for city in data))
     curs.close()
