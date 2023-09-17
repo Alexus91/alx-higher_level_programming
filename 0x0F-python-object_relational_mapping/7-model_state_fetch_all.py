5c3b886c6f3c8f943807809b993618fae1155778
Alexus91 <mrfadlkamal@gmail.com>
2023-09-17 12:51:05 -0700

    E128 continuation line under-indented for visual indent resolving

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,9 +7,10 @@ from sys import argv
 from model_state import Base, State
 
 if __name__ == "__main__":
-    engi = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
                                                                     argv[2],
                                                                     argv[3]))
+
     Base.metadata.create_all(engi)
     sess = sessionmaker(bind=engi)()
     for state in sess.query(State).order_by(State.id):
--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -7,10 +7,9 @@ from sys import argv
 from model_state import Base, State
 
 if __name__ == "__main__":
-    engi = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
                                                                     argv[2],
                                                                     argv[3]))
-
     Base.metadata.create_all(engi)
     sess = sessionmaker(bind=engi)()
     first_state = sess.query(State).order_by(State.id).first()
