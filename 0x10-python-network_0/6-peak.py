63a183a35aa1972465b6c99695dd0e74bfaf8b5b
Alexus91 <mrfadlkamal@gmail.com>
2023-09-28 11:51:43 -0700

    6. Find a peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Find Peak task 6
+"""
+
+def find_peak(list_of_integers):
+    if not list_of_integers:
+        return None
+
+    left, right = 0, len(list_of_integers) - 1
+
+    while left < right:
+        mid = (left + right) // 2
+
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            right = mid
+        else:
+            left = mid + 1
+
+    return list_of_integers[left]
