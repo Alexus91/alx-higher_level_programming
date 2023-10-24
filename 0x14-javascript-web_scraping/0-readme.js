73bd18cc50e314e275b7cd7c2d06d74a9b132a38
Alexus91 <mrfadlkamal@gmail.com>
2023-10-24 15:23:59 -0700

    0. Readme

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/*script that reads and prints the content of a file.*/
+
+const fs = require('fs');
+const name = process.argv[2];
+fs.readFile(name, 'utf8', (err, data) => {
+  if (err) console.log(err);
+  else console.log(data.toString());
+});
