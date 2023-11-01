31986d49284fca819282ba022b49dfa1a0365177
Alexus91 <mrfadlkamal@gmail.com>
2023-11-01 15:50:32 -0700

    7. Star wars character  6. Change the text  5. List of elements

--- 0x15-javascript-web_jquery/5-script.js
+++ 0x15-javascript-web_jquery/5-script.js
@@ -1,4 +1,4 @@
-// script that toggles the class of the <header> element when the user
+// script that toggles the class of the <header> element, when the user
 // clicks on the tag DIV#toggle_header
 
 $('div#toggle_header').click(function () {
